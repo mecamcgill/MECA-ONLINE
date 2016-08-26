0f2e2af932df627e8f393bfc528fb1edefa2bf10
mecamcgill <ilovemeca@gmail.com>
2016-08-25 20:16:10 -0400

    Add files via upload

--- js/smoothStateint.js
+++ js/smoothStateint.js
@@ -1,18 +1,52 @@
+$(function() {
+  $('#main').smoothState();
+    $('#main').smoothState({ prefetch: true });
+});
+
+$(function(){
+  'use strict';
+  var options = {
+    prefetch: true,
+    cacheLength: 2,
+    onStart: {
+      duration: 250, // Duration of our animation
+      render: function ($container) {
+        // Add your CSS animation reversing class
+        $container.addClass('is-exiting');
+
+        // Restart your animation
+        smoothState.restartCSSAnimations();
+      }
+    },
+    onReady: {
+      duration: 0,
+      render: function ($container, $newContent) {
+        // Remove your CSS animation reversing class
+        $container.removeClass('is-exiting');
+
+        // Inject the new content
+        $container.html($newContent);
+
+      }
+    }
+  },
+  smoothState = $('#main').smoothState(options).data('smoothState');
+});
+
+
 ;(function ($) {
   'use strict';
-  var content  = $('#main').smoothState({
-        // onStart runs as soon as link has been activated
+  var $body    = $('html, body'), // Define jQuery collection 
+      content  = $('#main').smoothState({
         onStart : {
-          
-          // Set the duration of our animation
           duration: 250,
-          
-          // Alterations to the page
           render: function () {
-
-            // Quickly toggles a class and restarts css animations
             content.toggleAnimationClass('is-exiting');
+            
+            // Scroll user to the top
+            $body.animate({ 'scrollTop': 0 });
+
           }
         }
-      }).data('smoothState'); // makes public methods available
+      }).data('smoothState');
 })(jQuery);
